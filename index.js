d2d6a3320df6937de7df3fe3b9ed6a1250414791
Marcelo Rocha <marcelo.ds.rocha@outlook.com>
2021-11-10 01:22:33 -0300

    Commentary added

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ buttons.forEach((elem)=>{
         info(elem.value);
     }
 });
+
+// Var for knowing the display state.
 let displayingResult = false;
+//Pick the pressed button and process
 const info = (number)=>{
     const regexOperations = /[+-\/*]/;
     const regexNumbers = /[\.0-9]/;
@@ -19,6 +22,7 @@ const info = (number)=>{
     
     //Keyboard press a number or a dot.
     if(regexNumbers.test(number)){
+        //If a number is pressed after a result, clear display;
         if(displayingResult){
             displayInput.innerHTML = '';
             displayResult.innerHTML = '';
@@ -27,13 +31,13 @@ const info = (number)=>{
         const dot = /\./;
         //if a dot already exist, dot button will do nothing
         if(number === '.' && dot.test(displayResult.innerHTML)){
-            console.log(dot.test(displayResult.innerHTML))
             return;
         }
         displayResult.innerHTML += number;
 
     //Pressing a operator
     }else{
+        //If a operator is pressed after a result, use it for new operation
         if(displayingResult){
             displayInput.innerHTML = displayResult.innerHTML;
             displayResult.innerHTML = '';
@@ -68,9 +72,10 @@ const info = (number)=>{
 
         //Pressing '='.
         }else{
+            //Put the last number in the account, calculate and display result;
             displayInput.innerHTML += displayResult.innerHTML;
             let result = calculate(displayInput.innerHTML);
-            if (result!='Infinity'){
+            if (result!='Infinity'){ //If value is finite, round it if necessary;
                 result = +result.toFixed(4);
             }
             displayResult.innerHTML = String(result);
@@ -84,19 +89,26 @@ const eraseOne = (numbers)=>{
     return numbers.slice(0,-1);
 };
 
+// - Pick the operation in string, divide it in all operators, separating the numbers.
+// - Then it veryfy if exist operators (if it is a valid entry).
+// - If valid, verify if the first string component is a minus signal.
+// - Separate primaryOperand (* and /), that need to be processed first;
+// - Separate the secondaryOperands (+ and -);
+// - Verify if primary exist, and process it, then the same thing for secondary;
+// - Return result.
 const calculate = (operation)=>{
     const regexOperations = /[+\-/*]/g;
     const firstLetter = operation.slice(0, 1);
     const numbers = operation.split(regexOperations).map((elem)=>Number(elem));
     const operators = operation.match(regexOperations);
     let i = 0;
-    if(operators != null){
+    if(operators != null){ //If non existent, go direct to result;
         if(firstLetter === '-'){
-            numbers[1] = - numbers[1];
-            operators.shift();
-            numbers.shift();
+            numbers[1] = - numbers[1]; //Turn first number negative and delete thresh data;
+            operators.shift();         //'-';
+            numbers.shift();           //0;
         }
-        let primaryOperands = [];
+        let primaryOperands = []; //Create a vector of ['operator', indexInOriginalOperationString].
         operators.forEach((elem, index)=>{
             if(elem === '/' || elem === '*'){
                 primaryOperands.push([elem, index]);
@@ -108,29 +120,29 @@ const calculate = (operation)=>{
                 secondaryOperands.push([elem, index]);
             }
         });
-        console.log(primaryOperands, 'fir')
-        console.log(secondaryOperands, 'sec')
-        console.log(secondaryOperands != '')
         let finishing = [];
         let correctFinishing = [];
-        if(primaryOperands != ''){
-            const firstMaths = prox(primaryOperands);
-            finishing = [...sequentialOperation(numbers, firstMaths)];
+        if(primaryOperands != ''){ //Exist * or /;
+            const firstMaths = prox(primaryOperands); //it create a 'Operation map'
+            finishing = [...sequentialOperation(numbers, firstMaths)]; //Operations * and / resolved;
             if(secondaryOperands != ''){
+                //Finishing just return the numbers that are processed, so isolated '+2-' need correction.
                 correctFinishing = completeMissing(finishing, numbers, secondaryOperands);
             }else{
+                //Non exist - and + operations to be made, return result;
                 correctFinishing = [...finishing];
-                console.log('Final result' , correctFinishing[0])
                 return correctFinishing[0];
             }
         }else{
+            //Non '' var, if * and / non exist
             correctFinishing = [...numbers];
         }
         if(secondaryOperands != ''){
-            console.log(correctFinishing , 'finishing');
+            //correcting index of operands, because of the new size of the operation
             const finishOperands = secondaryOperands.map((elem, index)=>{
                 return [elem[0], index];
             });
+            //convert it to OperationMap and send do sequencial calc.
             const lastMaths = prox(finishOperands);
             const result = sequentialOperation(correctFinishing, lastMaths);
             return result[0];
@@ -140,6 +152,8 @@ const calculate = (operation)=>{
     }else{return operation;}
 };
 
+
+//Receive two numbers and the respective operator.
 const singleOperation = (number1, operator, number2)=>{
     switch(operator){
         case '-':
@@ -155,8 +169,12 @@ const singleOperation = (number1, operator, number2)=>{
     }
 }
 
+
+//Receive an array with numbers to be processed and operation map.
 const sequentialOperation = (numbers, operationMap)=>{
     const results = [];
+    
+    //Add to results the needed operations in map.
     operationMap.forEach((direct)=>{
         const val = direct.reduce((value, opr)=>{
             return singleOperation(value, opr[0], numbers[opr[1]+1]);
@@ -166,50 +184,63 @@ const sequentialOperation = (numbers, operationMap)=>{
     return results;
 };
 
+
+//This function translate a vector of [operands, respectiveIndex]
+//In a vector with vectors of [operands, respectiveIndex].
+//Joining the near index together;
 const prox = (vector)=>{
     const finalVector = [];
     let line = [];
     for(let i = 0; i<vector.length; i++){
-        if(i==vector.length-1){
+        if(i==vector.length-1){ //Last operand
             line.push(vector[i]);
             finalVector.push(line);
             line = [];
-        }else if((vector[i][1]) == (vector[i+1][1])-1){
+        }else if((vector[i][1]) == (vector[i+1][1])-1){ //This index is near the next?
             line.push(vector[i]);
         }else{
+            //Non near and non last.
             line.push(vector[i]);
             finalVector.push(line);
             line = [];
         }
     }
-    console.log(finalVector);
     return finalVector;
 };
 
+
+//This function reconstructs the numbers processed with the non-processed ones.
+//This function pick an array with processed but missing numbers.
+//The inicial array of numbers.
+//The operands +,- and their index [[operand, index],[op,index]...]
 const completeMissing = (missing, numbers, operands)=>{
     let completeVector = [];
+    //.Shift would change missing in up code;
     const copyMissing = [...missing];
+
+    //If first operand index is 0, then a base number is missing
+    //If not, a already processed one is needed.
     if(operands[0][1] === 0){
         completeVector.push(numbers[0]);
     }else{
         completeVector.push(copyMissing.shift());
     }
+
+
     for(let i = 0; i<operands.length; i++){
-        if(i==operands.length-1){
+
+        if(i==operands.length-1){ //Last insertion
+            //Verify the last numbers is missing or already processed;
             if(operands[i][1] == ((numbers.length)-2)){
-                console.log(i)
                 completeVector.push(numbers[operands[i][1]+1])
             }else{
                 completeVector.push(copyMissing.shift());
             }
-        }else if((operands[i][1]) == (operands[i+1][1])-1){
-            completeVector.push(numbers[operands[i][1]+1])
+        }else if((operands[i][1]) == (operands[i+1][1])-1){ //This index is near the next?
+            completeVector.push(numbers[operands[i][1]+1])  //If it is, the next value is missing.
         }else{
-           completeVector.push(copyMissing.shift());
+           completeVector.push(copyMissing.shift()); //If it isn't, the next value already was processed.
         }
     }
     return completeVector;
 }
-
-//console.log('3*3+5-2+4*2+2');
-//completeMissing([ 9, 8 ], [ 3, 3, 5, 2, 4, 2, 2 ], [["+", 1],["-",2],["+",3],["+",5]]);
